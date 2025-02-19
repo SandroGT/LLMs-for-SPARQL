f461ee1af400c68503d077111eee3a9814c70d97
SandroGT <sandrogabrieletiddia@outlook.it>
2025-02-19 10:54:58 +0100

    DeepSeek-Coder-V2-Lite

--- experiments/run_llm_generation.py
+++ experiments/run_llm_generation.py
@@ -28,9 +28,9 @@ MAX_ERROR_STORE_LEN = 200
 MAX_QUERY_TIME = 5*60
 
 # LLM model
-LLM_MODEL_NAME = 'phi-4-14b'
+LLM_MODEL_NAME = 'deepseek-v2-coder-16b'
 LOGGER.info(f'Initializing LLM: {LLM_MODEL_NAME}')
-LLM_MODEL = TransformersLLM('microsoft/phi-4')
+LLM_MODEL = TransformersLLM('deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct')
 
 # File path settings
 SCRIPT_DIR = Path(__file__).parent.resolve()
