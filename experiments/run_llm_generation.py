79ca76f368303719907c80c4c5420b05f44adf1d
SandroGT <sandrogabrieletiddia@outlook.it>
2025-02-20 09:23:15 +0100

    CohereForAI c4ai-command-r-7b

--- experiments/run_llm_generation.py
+++ experiments/run_llm_generation.py
@@ -28,9 +28,9 @@ MAX_ERROR_STORE_LEN = 200
 MAX_QUERY_TIME = 5*60
 
 # LLM model
-LLM_MODEL_NAME = 'c4ai-command-r-32b'
+LLM_MODEL_NAME = 'c4ai-command-r-7b'
 LOGGER.info(f'Initializing LLM: {LLM_MODEL_NAME}')
-LLM_MODEL = TransformersLLM('CohereForAI/c4ai-command-r-08-2024')
+LLM_MODEL = TransformersLLM('CohereForAI/c4ai-command-r7b-12-2024')
 
 # File path settings
 SCRIPT_DIR = Path(__file__).parent.resolve()
